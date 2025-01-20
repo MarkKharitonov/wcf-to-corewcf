4789f71afe61da58af64b5cfc1a2e5ba09c2e72f
Mark Kharitonov <Mark.Kharitonov@dayforce.com>
2025-01-20 12:01:53 -0500

    Downgrade to .NET 5.0

--- modernized-wcf-service/CoreWCF.SampleService/Program.cs
+++ modernized-wcf-service/CoreWCF.SampleService/Program.cs
@@ -4,9 +4,12 @@
 using CoreWCF;
 using CoreWCF.Configuration;
 using CoreWCF.Description;
+using CoreWCF.SampleService;
 using CoreWCF.SampleService.BL;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Linq;
 using WCF.SampleService.Behaviors.ErrorBehavior;
 using WCF.SampleService.Behaviors.HeaderValidationBehavior;
@@ -14,54 +17,19 @@ using WCF.SampleService.Contracts;
 using WCF.SampleService.Loggers;
 using WCF.SampleService.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add WSDL support
-builder.Services.AddServiceModelServices().AddServiceModelMetadata();
-builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
-
-// Register dependencies
-builder.Services.AddSingleton<IFileLogger, FileLogger>(serviceProvider => new FileLogger(builder.Configuration["appSettings:logFilePath"]));
-builder.Services.AddSingleton<ISomeBL, SomeBL>();
-builder.Services.AddSingleton<CalculatorService>();
-builder.Services.AddSingleton<AuthService>();
-
-var app = builder.Build();
-
-app.UseServiceModel(builder =>
+internal class Program
 {
-    builder.AddService<AuthService>((serviceOptions) => { })
-    .AddServiceEndpoint<AuthService, IAuthService>(new BasicHttpBinding(), "/Services/AuthService.svc");
-
-    builder.AddService<CalculatorService>((serviceOptions) => { })
-    .AddServiceEndpoint<CalculatorService, ICalculatorService>(new BasicHttpBinding(), "Services/CalculatorService.svc");
-
-    builder.ConfigureAllServiceHostBase(serviceHost =>
+    private static void Main(string[] args)
     {
-        // Add service behaviors
-        var fileLogger = app.Services.GetRequiredService<IFileLogger>();
-        serviceHost.Description.Behaviors.Add(new ErrorBehavior(typeof(GlobalErrorHandler), fileLogger));
+        var hostBuilder = Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 
-        // Add endpoint behaviors
-        serviceHost.Description.Endpoints.ToList().ForEach(endpoint =>
-        {
-            endpoint.EndpointBehaviors.Add(new HeaderValidationBehavior());
-        });
+        var host = hostBuilder.Build();
 
-        var debug = serviceHost.Description.Behaviors.Find<ServiceDebugBehavior>();
-
-        if (debug == null)
-        {
-            serviceHost.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
-        }
-        else
-        {
-            debug.IncludeExceptionDetailInFaults = true;
-        }
-    });
-});
-
-var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
-serviceMetadataBehavior.HttpGetEnabled = true;
-
-app.Run();
+        host.Run();
+    }
+}
---
+++ modernized-wcf-service/CoreWCF.SampleService/Startup.cs
@@ -0,0 +1,84 @@
+﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+using CoreWCF.Configuration;
+using CoreWCF.Description;
+using CoreWCF.SampleService.BL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using WCF.SampleService.Behaviors.ErrorBehavior;
+using WCF.SampleService.Behaviors.HeaderValidationBehavior;
+using WCF.SampleService.Contracts;
+using WCF.SampleService.Loggers;
+using WCF.SampleService.Services;
+
+namespace CoreWCF.SampleService
+{
+    public class Startup
+    {
+        private readonly IConfiguration m_configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            m_configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceModelServices();
+            services.AddServiceModelMetadata();
+            services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+            // Register dependencies
+            services.AddSingleton<IFileLogger, FileLogger>(serviceProvider => new FileLogger(m_configuration["appSettings:logFilePath"]));
+            services.AddSingleton<ISomeBL, SomeBL>();
+            services.AddSingleton<CalculatorService>();
+            services.AddSingleton<AuthService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseServiceModel(builder =>
+            {
+                builder.AddService<AuthService>((serviceOptions) => { });
+                builder.AddServiceEndpoint<AuthService, IAuthService>(new BasicHttpBinding(), "/Services/AuthService.svc");
+
+                builder.AddService<CalculatorService>((serviceOptions) => { });
+                builder.AddServiceEndpoint<CalculatorService, ICalculatorService>(new BasicHttpBinding(), "Services/CalculatorService.svc");
+
+                builder.ConfigureAllServiceHostBase(serviceHost =>
+                {
+                    // Add service behaviors
+                    var fileLogger = app.ApplicationServices.GetRequiredService<IFileLogger>();
+                    serviceHost.Description.Behaviors.Add(new ErrorBehavior(typeof(GlobalErrorHandler), fileLogger));
+
+                    // Add endpoint behaviors
+                    serviceHost.Description.Endpoints.ToList().ForEach(endpoint =>
+                    {
+                        endpoint.EndpointBehaviors.Add(new HeaderValidationBehavior());
+                    });
+
+                    var debug = serviceHost.Description.Behaviors.Find<ServiceDebugBehavior>();
+
+                    if (debug == null)
+                    {
+                        serviceHost.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+                    }
+                    else
+                    {
+                        debug.IncludeExceptionDetailInFaults = true;
+                    }
+                });
+            });
+
+            var serviceMetadataBehavior = app.ApplicationServices.GetRequiredService<ServiceMetadataBehavior>();
+            serviceMetadataBehavior.HttpGetEnabled = true;
+        }
+    }
+}
